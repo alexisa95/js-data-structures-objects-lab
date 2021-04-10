e1363c43e23b26fb4e9f8cb7a81a250b5035f64f
Alexis <lexisadams92@gmail.com>
2021-04-09 21:36:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const driver = {name: 'Akarsh Shukla', streetAddress: '6322 field flower trail'}
+
+function updateDriverWithKeyAndValue(obj, key, value) {
+    const nextDriver = {...obj};
+    
+    nextDriver[key] = value;
+    return nextDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromDriverByKey(obj, key) {
+    const driver3 = {...obj};
+    delete driver3[key];
+    return driver3;
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
